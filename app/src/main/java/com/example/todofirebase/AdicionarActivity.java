9d305449ae13ba37019caf2b0230ec803e006988
leandroyellow <leandro.biologo@yahoo.com.br>
2019-05-09 11:45:34 -0300

    firebase concluido

--- app/src/main/java/com/example/todofirebase/AdicionarActivity.java
+++ app/src/main/java/com/example/todofirebase/AdicionarActivity.java
@@ -2,12 +2,57 @@ package com.example.todofirebase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.example.todofirebase.modelo.Tarefa;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.UUID;
 
 public class AdicionarActivity extends AppCompatActivity {
+
+    EditText editTextNome;
+    private Tarefa tarefa;
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adicionar);
+
+        tarefa = new Tarefa();
+        tarefa.setUid(this.getIntent().getStringExtra("uid"));
+        tarefa.setNome(this.getIntent().getStringExtra("nome"));
+
+        editTextNome = findViewById(R.id.txtNome);
+        editTextNome.setText(tarefa.getNome());
+        conectarBanco();
+
     }
+
+    public void conectarBanco(){
+        FirebaseApp.initializeApp(AdicionarActivity.this);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+    }
+
+    public void salvarTarefa(View view){
+        Tarefa tarefa = new Tarefa();
+        if (this.getIntent().getStringExtra("uid")==null){
+            tarefa.setUid(UUID.randomUUID().toString());
+        }
+        else{
+            tarefa.setUid(this.getIntent().getStringExtra("uid"));
+        }
+
+        tarefa.setNome(editTextNome.getText().toString());
+        databaseReference.child("Tarefa").child(tarefa.getUid()).setValue(tarefa);
+        finish();
+    }
+
 }
